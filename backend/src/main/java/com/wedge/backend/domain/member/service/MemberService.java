8a2ebfd18ec7d3d01613148745992620e42121a8
개발자1 <cm41823@gmail.com>
2026-06-12 15:06:10 +0900

    Style : 주석 추가

--- backend/src/main/java/com/wedge/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/wedge/backend/domain/member/service/MemberService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
+    //내부 개발용 회원 조회 메서드 정보, 프론트에서 사용X
     public Member getMember(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
