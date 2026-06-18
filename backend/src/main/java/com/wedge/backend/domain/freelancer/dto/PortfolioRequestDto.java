18d825f2a9a60ff2acf34e34e84c2ff2a85e3130
minseo <p7548296@gmail.com>
2026-06-18 10:18:01 +0900

    Fix : PortfolioRequestDto @Setter 추가로 @ModelAttribute 바인딩 버그 수정

--- backend/src/main/java/com/wedge/backend/domain/freelancer/dto/PortfolioRequestDto.java
+++ backend/src/main/java/com/wedge/backend/domain/freelancer/dto/PortfolioRequestDto.java
@@ -3,8 +3,10 @@ package com.wedge.backend.domain.freelancer.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PortfolioRequestDto {
